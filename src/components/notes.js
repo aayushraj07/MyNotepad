2271e554527c265a9d03c59ccdc215ab08631662
aayushraj07 <aayush.97raj@gmail.com>
2020-05-18 22:19:08 +0530

    First submit error solved and props passing to notelist.js

--- src/components/notes.js
+++ src/components/notes.js
@@ -1,38 +1,40 @@
 import React,{Component} from 'react';
 import '../App.css';
 import Editor from './editor';
-import Item from './item';
-
+// import NoteList from './notelist';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+    state = {
       notes:[
         {title:"First Note",content:"This one is for trial"}
     ],
     count: 0
     }
-    this.handleInputValue = this.handleInputValue.bind(this);
-  }
+  
  
   //Callback function which is also adding data from child component
-  handleInputValue(val) {
+  handleInputValue=(val)=> {
     this.setState(state=>{
       const notes = state.notes.concat(val);
       return{
         notes
       }
-    })
+    },()=>console.log(this.state)
+    )
     this.setState({ count: this.state.count+1});
     console.log(this.state)
+    // debugger;
   }
  
   render() {
     return (
       <div className="container-fluid">
+        <div className="row">
         <Editor handleInput={this.handleInputValue} />
-        <Item dataToShow={this.notes[this.state.count]} />
+        </div>
+        <div className="row">
+            <NoteList dataToShow={this.state.notes[0]} />  
+        </div>        
       </div>
     );
   }
@@ -40,36 +42,3 @@ class App extends Component {
  
 export default App;
 
-
-// import React,{Component} from 'react';
-// import Editor from './editor';
-// // import Item from './item';
-
-// class Notes extends Component{
-//     constructor(props){
-//         super(props);
-//         this.state={
-//             title:''
-//             // notes:
-//             // [
-//             //     {title:"Aayush Raj" , content:"developer"},
-//             //     {title:"Honey" , content:"Engineer"}
-//             // ]
-//         }
-//         this.callbackFun= this.callbackFun.bind(this)
-//     }
-
-//     callbackFun(){
-//         // this.setState ({title:obje});    
-//         console.log("Working")
-//     }
-//     render(){
-//         return(
-//             <div className="container-fluid">
-//                 <Editor handleInput={this.callbackFun}/>
-//             </div>
-//         )
-//     }
-// }
-
-// export default Notes;
---
+++ src/components/noteslist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NoteList=(props)=>{
+
+
+    return(
+        <div>
+            {this.props.dataToShow.title}    
+        </div>
+    )
+}
+
+export default NoteList;
